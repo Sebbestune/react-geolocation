7dfe41edd4538a288c0813f9bafc326762b1c65b
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2023-04-19 12:25:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser!");
+    } else {
+      setStatus("Loading...");
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setStatus("");
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+      },
+      () => {
+        setStatus("Unable to retrieve your location");
+      }
+    );
+  };
+
+  return (
+    <div className="App">
+      <button onClick={() => getLocation()}>Get Location</button>
+      {status ? <p>Status: {status}</p> : <></>}
+      <p>Latitude: {lat}</p>
+      <p>Longitude: {lng}</p>
+    </div>
+  );
+}
+
+export default App;
